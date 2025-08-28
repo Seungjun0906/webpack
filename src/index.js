0b93d9406c4f56567179024cbeb7b0e19eb90d7a
Jun <87404676+Seungjun0906@users.noreply.github.com>
2025-08-28 14:21:59 +0900

    이미지 처리 설정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from "lodash";
 import "./style.css";
+import Icon from "./info.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack", " "]);
-
   element.classList.add("hello");
+
+  // 기존 div에 Icon 추가하기
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
