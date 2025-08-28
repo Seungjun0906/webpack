b8564e9de7eb246b8be6cbab24c9ae3a70c988f7
Jun <87404676+Seungjun0906@users.noreply.github.com>
2025-08-28 14:41:44 +0900

    폰트 추가하기

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ import Icon from "./info.png";
 function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack", " "]);
-  element.classList.add("hello");
+  const span = document.createElement("span");
+
+  span.innerHTML = "hello";
+  span.classList.add("hello");
+
+  element.appendChild(span);
+
+  const span2 = document.createElement("span");
+  span2.innerHTML = "wrold";
+  span2.classList.add("world");
+
+  element.appendChild(span2);
+
+  // element.innerHTML = _.join(["Hello", "webpack", " "]);
+  // element.classList.add("hello");
 
   // 기존 div에 Icon 추가하기
   const myIcon = new Image();
